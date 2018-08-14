a423c67aeed3315f787150e5c1b63086d5a7232d
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-08-14 15:13:21 -0500

    buscando en carpetas directorios y archivos

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 #!/usr/bin/env node
-let request = require('request');
 let fetch = require('node-fetch');
+let fs = require('fs');
+const path = require('path');
 
-// const mdLinks = (path, options) => {
-//
-// }
-// module.exports = mdLinks;
-// options = {
-//   validate : true,
-//   stats : true
-// };
+var dir = '..';
+fs.readdir(dir, (err, files) => {
+	var r = [];
+	files.forEach((file) => {
+		s(file);
+		function s(file){
+			fs.stat(dir + '/' + file, (err, stat) => {
+				if(err){console.error(err);return;}
+				if(stat.isDirectory())r.push({f:file, type: 'dir'});
+				else if(stat.isFile())r.push({f:file, type: 'file'});
+				else r.push(0);
+				if(r.length == files.length){
+					r.filter((m) => {return m;});
+					console.log(r);
+				}
+			});
+		}
+	});
+});
+const mdLinks = (dir) => {
+  let stats = fs.statSync(dir);
+  if(stats.isFile()) {
+    var valor = dir,
+        archivo = valor.substr(valor.lastIndexOf("\\") + 1);
+    if (/[a-z\d\-_\.]+\.md/gi.test(archivo))
+        console.log("Es un archivo markdown");
+    else
+        console.log("No es un archivo markdown");
+
+  } else {
+    console.log('is a directory')
+  }
+}
+options = {
+  validate : true,
+  stats : true
+};
+mdLinks(dir);
 urlLinks = ['https://www.google.com', 'https://www.facebook.com'];
 urlLinks.forEach(function(url) {
   fetch(url)
@@ -20,3 +51,4 @@ urlLinks.forEach(function(url) {
       console.log(status);
     });
 });
+// module.exports = mdLinks;
