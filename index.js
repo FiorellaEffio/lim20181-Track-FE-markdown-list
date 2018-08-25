e304b17e526fec9b8b82f8fabaded39570bc0c5a
FiorellaEffio <fiorella.effio@unmsm.edu.pe>
2018-08-25 15:44:25 -0500

    archivos md listos

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-
+let lineNumber = 0;
+lineReader.eachLine(path, function(line, last) {
+  lineNumber++;
+  url = line.match(/\[([^\]]+)\]\(([^)]+)\)/);
+  if(url !== null) {
+    file = path;
+    text = url[1]
+    href = url[2];
+    links.push({lineNumber, text, href});
+    let fileStats = {file, links};
+    console.log(fileStats)
+    files.push(fileStats);
+    console.log(files)
+  }
+  if (last) {
+    return false; // stop reading
+  }
+});
 
 let links = [{href:'https://www.google.com.pe', lineNumber: 3, file: 'test'},
   {href:'https://developer.mozilla.org/es/docs/Web', lineNumber:7,file:'test'}];
--- index3.js
+++ index3.js
@@ -1,26 +1,55 @@
 var fs = require('fs');
 var path = require('path');
+let lineReader = require('line-reader');
+let fetch = require('node-fetch');
 
-function getFilesFromDir(dir) {
+const validatePath = (path) => {
+  try {
+    require('fs').accessSync(path)
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+const getFilesFromDir = (dir) => {
   var filesToReturn = [];
   function walkDir(currentPath) {
-    console.log(currentPath)
     var files = fs.readdirSync(currentPath);
     for (var i in files) {
-      console.log(currentPath)
-      console.log(files[i])
       var currentFile = path.join(currentPath, files[i]);
-      console.log(path.extname(currentFile))
       if (fs.statSync(currentFile).isFile() && path.extname(currentFile) === '.md') {
-        filesToReturn.push(currentFile.replace(dir, ''));
+        filesToReturn.push({file:currentFile});
       } else if (fs.statSync(currentFile).isDirectory()) {
        walkDir(currentFile);
       }
     }
   };
-  walkDir(dir);
+  if(fs.statSync(dir).isFile()) {
+    if(path.extname(dir) === '.md') {
+      filesToReturn.push({file:dir});
+    } else {
+      console.log('No es archivo markdown');
+    }
+  } else {
+    walkDir(dir);
+  }
   return filesToReturn;
 }
 
+const mdLinks = (path, options) => {
+ options = (options) ? options : {validate:false, stats:false};
+ return new Promise((resolved, reject) => {
+   if(validatePath(path)) {
+     files = getFilesFromDir(path);
+     resolved(files);
+   } else {
+     resolved('ruta no valida');
+   }
+ })
+}
 //print the txt files in the current directory
-console.log(getFilesFromDir("test"));
+let options = {validate:false,stats:false}
+mdLinks('test', options)
+ .then((files)=> console.log(files))
+module.exports = mdLinks;
