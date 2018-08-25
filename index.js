89dba618c152b07ade20a2272601657cc546b383
FiorellaEffio <fiorella.effio@unmsm.edu.pe>
2018-08-25 16:03:08 -0500

    d

--- index.js
+++ index.js
@@ -17,17 +17,6 @@ lineReader.eachLine(path, function(line, last) {
   }
 });
 
-let links = [{href:'https://www.google.com.pe', lineNumber: 3, file: 'test'},
-  {href:'https://developer.mozilla.org/es/docs/Web', lineNumber:7,file:'test'}];
-
-function getLinks() {
-  return new Promise((resolved, reject) => {
-    setTimeout(()=>{
-      resolved(links)
-    }, 1000)
-  })
-}
-
 function getStatusCode(links) {
   return new Promise((resolved, reject) => {
     links.forEach(function(element) {
